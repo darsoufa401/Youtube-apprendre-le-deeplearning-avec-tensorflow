cf390f395ab6989d4ca13ce7a538fc175056dd84
darsoufa401 <152397480+darsoufa401@users.noreply.github.com>
2023-11-29 19:09:13 +0100

    Delete #4 - CNN directory

--- #4 - CNN/download_images.py
+++
@@ -1,6 +0,0 @@
-from google_images_download import google_images_download   #importing the library
-
-response = google_images_download.googleimagesdownload()   #class instantiation
-
-arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"format":"jpg"}
-paths = response.download(arguments)
